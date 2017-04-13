86dc8fb3c93bfd11466e111993a695a2fbd257a8
changjianwang <changjianwang@cyou-inc.com>
2017-04-13 17:31:48 +0800

    mutation 必须是同步函数

--- src/views/ex12-vuex-mutations.vue
+++ src/views/ex12-vuex-mutations.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <p>mutations</p>
+    <p>mutation 必须是同步函数</p>
     <p>更改 Vuex 的 store 中的状态的唯一方法是提交 mutation。Vuex 中的 mutations 非常类似于事件：每个 mutation 都有一个字符串的 事件类型 (type) 和 一个 回调函数 (handler)。这个回调函数就是我们实际进行状态更改的地方，并且它会接受 state 作为第一个参数：</p>
     <p>count: {{ count }}</p>
     <p>
