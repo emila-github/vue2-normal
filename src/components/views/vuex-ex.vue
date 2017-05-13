5d65aa628c3b170093245953eeef9394c7ed2f75
changjianwang <changjianwang@cyou-inc.com>
2017-05-13 10:44:47 +0800

    添加vuex安装方法

--- src/components/views/vuex-ex.vue
+++ src/components/views/vuex-ex.vue
@@ -6,6 +6,27 @@
       <li><a href="http://vuex.vuejs.org/zh-cn/" target="_blank">Vuex</a></li>
     </ul>
 
+    <h2>安装</h2>
+    <h3>npm</h3>
+    <div class="tip">
+      npm install vuex --save
+    </div> 
+    <h3>yarn</h3>
+    <div class="tip">
+      yarn add vuex
+    </div>
+
+    <p>通过 Vue.use() 来安装 Vuex, 在入口文件main.js中添加如下代码</p>
+    <div class="tip">
+      <pre>
+      import Vue from 'vue'
+      import Vuex from 'vuex'
+
+      Vue.use(Vuex)
+      </pre>
+    </div>
+    
+
   </div>
 </template>
 
